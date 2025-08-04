9d17d12f2837f34b0fb700e4fc2efa1a12ae9088
Rohan16298 <urk3hyd@bosch.com>
2025-08-04 09:37:22 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,48 +1,40 @@
-#include <assert.h>
 #include <iostream>
+#include <map>
+#include <functional>
 using namespace std;
 
-enum CheckType {
-  TEMPERATURE,
-  SOC,
-  CHARGE_RATE
-};
+enum CheckType { TEMPERATURE, SOC, CHARGE_RATE };
+
+bool checkRange(float value, float min, float max, const string& errorMsg) {
+    if (value < min || value > max) {
+        cout << errorMsg << "\n";
+        return false;
+    }
+    return true;
+}
 
 bool checkParameter(CheckType check, float value) {
-  switch (check) {
-    case TEMPERATURE:
-      if (value < 0 || value > 45) {
-        cout << "Temperature out of range!\n";
-        return false;
-      }
-      break;
-    case SOC:
-      if (value < 20 || value > 80) {
-        cout << "State of Charge out of range!\n";
-        return false;
-      }
-      break;
-    case CHARGE_RATE:
-      if (value > 0.8) {
-        cout << "Charge Rate out of range!\n";
-        return false;
-      }
-      break;
-    default:
-      // Unknown check type
-      return false;
-  }
-  return true;
+    static const map<CheckType, function<bool(float)>> checkers = {
+        {TEMPERATURE, [](float v) { return checkRange(v, 0, 45, "Temperature out of range!"); }},
+        {SOC, [](float v) { return checkRange(v, 20, 80, "State of Charge out of range!"); }},
+        {CHARGE_RATE, [](float v) { return checkRange(v, 0, 0.8, "Charge Rate out of range!"); }}
+    };
+
+    auto it = checkers.find(check);
+    if (it != checkers.end()) {
+        return it->second(value);
+    }
+    // Unknown check type
+    return false;
 }
 
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
-  if (!checkParameter(TEMPERATURE, temperature)) return false;
-  if (!checkParameter(SOC, soc)) return false;
-  if (!checkParameter(CHARGE_RATE, chargeRate)) return false;
-  return true;
+    return checkParameter(TEMPERATURE, temperature) &&
+           checkParameter(SOC, soc) &&
+           checkParameter(CHARGE_RATE, chargeRate);
 }
 
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+    assert(batteryIsOk(25, 70, 0.7) == true);
+    assert(batteryIsOk(50, 85, 0) == false);
 }
